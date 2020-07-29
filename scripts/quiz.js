c954c7282f966619366b99e1e631a5dea949a4a6
phdfreitas <pedro.hnq.fraga@gmail.com>
2020-07-29 12:27:19 -0300

    Alterando Layout e Refatorando quiz.js

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,13 +1,15 @@
 let perguntas = Array(
 	'Quando o JavaScript foi criado?',
 	'Quem foi o criador do JavaScript?',
-	'Há quantos anos surgiu a linguagem de programação "Python"?'
+	'Há quantos anos surgiu a linguagem de programação "Python"?',
+	''
 )
 
 let respostas = Array(
 	Array('04/12/1995', '12/04/1997', '06/07/2000', '12/12/1996'),
 	Array('Bill Gates', 'Post Malone', 'Brendan Eich', 'James Gosling'),
 	Array('22 anos', '28 anos', '18 anos', '30 anos'), 
+	Array('', '', '', '') 
 )
 
 
@@ -23,7 +25,7 @@ let respostas_jogador = Array()
 let respostas_corretas = Array('letra_A', 'letra_C', 'letra_B')
 
 
-function screenRender(){
+function game(letra){
 
 	questao_atual.innerHTML = contador + 1
 	questao.innerHTML = perguntas[contador]
@@ -32,19 +34,28 @@ function screenRender(){
 	letra_C.innerHTML = respostas[contador][2]
 	letra_D.innerHTML = respostas[contador][3] 
 
+
+	if(letra != undefined) respostas_jogador.push(letra)
+
 	contador++
 
-	if(contador === 3){
-		contador = 1
+	if(contador > 3){
+		
+		gameOver()
+		
+	}
+}
+game()
+
+function gameOver(){
+	var playerScore = 0
+
+	for (var i = 0; i < 3; i++) {
+		if(respostas_corretas[i] === respostas_jogador[i]){
+			playerScore++
+		}
 	}
 
+	console.log(playerScore)
+		
 }
-screenRender()
-
-function playerAnswers(letra){
-	
-	respostas_jogador.push(letra)
-	console.log(respostas_jogador)
-
-}
-
